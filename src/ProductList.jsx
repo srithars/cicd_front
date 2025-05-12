633d1238a0e644d8cdded65469d8e78547a91842
srithars <64460687+srithars@users.noreply.github.com>
2025-05-12 12:03:38 +0530

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function ProductList({ products }) {
   return (
     <div>
-      <h2>Product List</h2>
+      <h2>Product List KLU</h2>
       {products.length === 0 ? (
         <p>No products found.</p>
       ) : (
